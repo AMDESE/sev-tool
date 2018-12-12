e87309cfc85e452bf6565dfd8c87a64785815a4e
Larry Dewey <ldewey@suse.com>
2018-12-12 15:19:03 -0700

    build: making the build script distribution agnostic
    Signed-off-by: Larry Dewey <ldewey@suse.com>

--- build.sh
+++ build.sh
@@ -5,8 +5,49 @@
 old_dir=$(pwd)
 echo $old_dir
 
-# install libelf to be able to correctly build kernel modules/`uname
-sudo apt --assume-yes install libelf-dev
+# Regular Expression to read /etc/os-release for major distributions.
+ID_RE='ID_\?(LIKE)\?\=\"\?[[:alnum:]]\+[[:space:]]\?[[:alnum:]]\+\?\"\?'
+
+# Grab only the value in between ID_LIKE or ID.
+REPLACE_RE='s/ID_\?\(LIKE\)\?=\"\(.*\)\"/\2/'
+
+# Read /etc/os-release to find the distribution base.
+DIST_BASE=$(cat /etc/os-release | grep "${ID_RE}" | sed "${REPLACE_RE}")
+
+# install libelf to be able to correctly build kernel modules/`uname,
+# but only if it isn't already installed.
+if [[ $DIST_BASE =~ 'suse' ]]
+then
+    # Cover all SLE and openSUSE distributions.
+
+    if [[ $(rpm -q libelf-devel 2>&1) =~ "not installed" ]]
+    then
+        sudo zypper in --non-interactive libelf-devel
+    else
+        echo "libelf libraries already installed, skipping installation."
+    fi
+elif [[ $DIST_BASE =~ 'debian' ]] || [[ $DIST_BASE =~ 'ubuntu' ]]
+then
+    # Cover all Debian and Ubuntu based distributions.
+
+    if [[ $(dpkg -l libelf-dev 2>&1) =~ "no packages found" ]]
+    then
+        sudo apt-get -y install libelf-dev
+    else
+        echo "libelf libraries already installed, skipping installation."
+    fi
+elif [[ $DIST_BASE =~ 'fedora' ]] || [[ $DIST_BASE =~ 'rhel' ]]
+then
+    # Cover all Redhat based distributions.
+
+    if [[ $(rpm -q elfutils-libelf-devel 2>&1) =~ "not installed" ]]
+    then
+        # Using yum as this will be used for CentOS.
+        sudo yum install -y elfutils-libelf-devel
+    else
+        echo "libelf libraries already installed, skipping installation."
+    fi
+fi
 
 # Fetch openssl submodule
 git submodule init
