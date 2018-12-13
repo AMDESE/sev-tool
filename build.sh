61511d3822e26af8024d30fb55082b0cbfd58f2c
Larry Dewey <ldewey@suse.com>
2018-12-13 09:42:54 -0700

    build: Fixed issue with regular expression.
    There were some missing parenthesis on the capture group, so those were
    added to fix the matching. I also added debugging logic to help with
    debugging int he future. The interpreter is now explicitly specified as
    ubuntu uses dash as the default in place of bash, which causes issues.
    
    Signed-off-by: Larry Dewey <ldewey@suse.com>

--- build.sh
+++ build.sh
@@ -1,18 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 # Script to build AMD SEV Tool
 
+# Set to 1 to enable debugging
+DEBUGGING=0
+
+if [ $DEBUGGING -eq 1 ]
+then
+  debug()
+  {
+    echo ""
+    echo $@
+  }
+else
+  debug()
+  {
+    echo -n ""
+  }
+fi
+
 # save the current directory to we can go back to it at the end
-old_dir=$(pwd)
-echo $old_dir
+OLD_DIR=$(pwd)
+debug "OLD_DIR => " ${OLD_DIR}
+
+OS_RELEASE=$(cat /etc/os-release)
+debug "OS_RELEASE => " ${OS_RELEASE}
 
 # Regular Expression to read /etc/os-release for major distributions.
-ID_RE='ID_\?(LIKE)\?\=\"\?[[:alnum:]]\+[[:space:]]\?[[:alnum:]]\+\?\"\?'
+ID_RE='ID\(\_LIKE\)\?\=\"\?[[:alpha:]]\+\([[:space:]][[:alpha:]]\*\)\?\"\?'
 
-# Grab only the value in between ID_LIKE or ID.
-REPLACE_RE='s/ID_\?\(LIKE\)\?=\"\(.*\)\"/\2/'
+debug "ID_RE" ${ID_RE}
 
 # Read /etc/os-release to find the distribution base.
-DIST_BASE=$(cat /etc/os-release | grep "${ID_RE}" | sed "${REPLACE_RE}")
+DIST_BASE=$(cat /etc/os-release | grep "${ID_RE}")
+
+debug "DIST_BASE =>" ${DIST_BASE}
 
 # install libelf to be able to correctly build kernel modules/`uname,
 # but only if it isn't already installed.
@@ -20,8 +41,10 @@ if [[ $DIST_BASE =~ 'suse' ]]
 then
     # Cover all SLE and openSUSE distributions.
 
+    debug "Distribution recognized as SUSE based"
     if [[ $(rpm -q libelf-devel 2>&1) =~ "not installed" ]]
     then
+        echo "Missing libelf library dependency. Installing now..."
         sudo zypper in --non-interactive libelf-devel
     else
         echo "libelf libraries already installed, skipping installation."
@@ -29,9 +52,10 @@ then
 elif [[ $DIST_BASE =~ 'debian' ]] || [[ $DIST_BASE =~ 'ubuntu' ]]
 then
     # Cover all Debian and Ubuntu based distributions.
-
+    debug "Distribution recognized as Debian or Ubuntu based"
     if [[ $(dpkg -l libelf-dev 2>&1) =~ "no packages found" ]]
     then
+        echo "Missing libelf library dependency. Installing now..."
         sudo apt-get -y install libelf-dev
     else
         echo "libelf libraries already installed, skipping installation."
@@ -39,10 +63,11 @@ then
 elif [[ $DIST_BASE =~ 'fedora' ]] || [[ $DIST_BASE =~ 'rhel' ]]
 then
     # Cover all Redhat based distributions.
-
+    debug "Distribution recognized as Fedora or Rhel based"
     if [[ $(rpm -q elfutils-libelf-devel 2>&1) =~ "not installed" ]]
     then
         # Using yum as this will be used for CentOS.
+        echo "Missing libelf library dependency. Installing now..."
         sudo yum install -y elfutils-libelf-devel
     else
         echo "libelf libraries already installed, skipping installation."
@@ -59,7 +84,7 @@ cd openssl/
 make -j64
 cd ../
 
-# Rename SEV Tool binary
+# Rebuild SEV Tool binary
 cd src/
 make clean
 make -j64
