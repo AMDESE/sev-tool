e47bdc7b5e58683c9bca6a068345a7fa23da4b37
Larry Dewey <ldewey@suse.com>
2019-02-22 12:18:17 -0700

    Implementing better handle of dependencies
    Signed-off-by: Larry Dewey <ldewey@suse.com>

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@
 # Set to 1 to enable debugging
 DEBUGGING=0
 
+# If dependancies are needed
+NEED_DEPS=0
+INSTALLER=""
+LIBELF_NAME=""
+DEPENDENCIES=(git make gcc zip)
+
 if [ ${DEBUGGING} -eq 1 ]
 then
   debug()
@@ -35,53 +41,69 @@ DIST_BASE=$(cat /etc/os-release | grep "${ID_RE}")
 
 debug "DIST_BASE =>" ${DIST_BASE}
 
-# install libelf to be able to correctly build kernel modules/`uname,
-# but only if it isn't already installed.
 if [[ ${DIST_BASE} =~ 'suse' ]]
 then
     # Cover all SLE and openSUSE distributions.
-
     debug "Distribution recognized as SUSE based"
-    if [[ $(rpm -q libelf-devel 2>&1) =~ "not installed" ]]
-    then
-        echo "Missing libelf library dependency. Installing now..."
-        sudo zypper in --non-interactive libelf-devel
-    else
-        echo "libelf libraries already installed, skipping installation."
-    fi
+
+    ${INSTALLER}="zypper"
+    ${LIBELF_NAME}="libelf-devel"
+    ${GCC_CPP}="gcc-c++"
 elif [[ ${DIST_BASE} =~ 'debian' ]] || [[ ${DIST_BASE} =~ 'ubuntu' ]]
 then
     # Cover all Debian and Ubuntu based distributions.
     debug "Distribution recognized as Debian or Ubuntu based"
-    if [[ $(dpkg -l libelf-dev 2>&1) =~ "no packages found" ]]
-    then
-        echo "Missing libelf library dependency. Installing now..."
-        sudo apt-get -y install libelf-dev
-    else
-        echo "libelf libraries already installed, skipping installation."
-    fi
-    if [[ $(dpkg -l zip 2>&1) =~ "no packages found" ]]
-    then
-        echo "Missing zip library dependency. Installing now..."
-        sudo apt-get -y install zip
-    else
-        echo "zip libraries already installed, skipping installation."
-    fi
+
+    ${INSTALLER}="apt-get"
+    ${LIBELF_NAME}="libelf-dev"
+    ${GCC_CPP}="g++"
 elif [[ ${DIST_BASE} =~ 'fedora' ]] || [[ ${DIST_BASE} =~ 'rhel' ]]
 then
     # Cover all Redhat based distributions.
     debug "Distribution recognized as Fedora or Rhel based"
-    if [[ $(rpm -q elfutils-libelf-devel 2>&1) =~ "not installed" ]]
-    then
-        # Using yum as this will be used for CentOS.
-        echo "Missing libelf library dependency. Installing now..."
-        sudo yum install -y elfutils-libelf-devel
-    else
-        echo "libelf libraries already installed, skipping installation."
-    fi
+
+    ${INSTALLER}="yum"
+    ${LIBELF_NAME}="elfutils-libelf-devel"
+    ${GCC_CPP}="gcc-c++"
 else
     debug "Regular expression could not match: \n" "${OS_RELEASE}"
-    echo "Distribution not recognized. Please manually install libelf libraries."
+    echo "Distribution not recognized. Please manually install "\
+         "libelf libraries, make, zip, gcc, g++, and git." >&2
+    exit 1
+fi
+
+debug "Checking for dependencies..."
+
+# Check for all required dependancies
+if [[ ${INSTALLER} = "zypper" ]] || [[ ${INSTALLER} = "yum" ]]
+then
+    for package in ${DEPENDENCIES[@]}
+    do
+        if [[ $(rpm -q ${package} 2>&1) =~ "not installed" ]]
+        then
+            ${NEED_DEPS}=1
+            break
+        fi
+    done
+elif [[ ${INSTALLER} = "apt-get" ]]
+then
+    for package in ${DEPENDENCIES[@]}
+    do
+        if [[ $(dpkg -l ${package} 2>&1) =~ "no packages found" ]]
+        then
+            ${NEED_DEPS}=1
+            break
+        fi
+    done
+fi
+
+# Install dependencies if they are needed.
+if [[ ${NEED_DEPS} -eq 1 ]]
+then
+    debug "A dependency is missing, installing now."
+    debug "Running Command: \"sudo ${INSTALLER} -y install "\
+          "${LIBELF_NAME} ${DEPENDENCIES[@]} ${GCC_CPP}\""
+    sudo ${INSTALLER} -y install ${LIBELF_NAME} ${DEPENDENCIES[@]} ${GCC_CPP}
 fi
 
 # Fetch openssl submodule
