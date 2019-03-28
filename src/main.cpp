55f9b13654619521b3531b312bc319babeb18801
Brian Lilienthal <Brian.Lilienthal@amd.com>
2019-03-28 15:18:06 -0500

    Updating the comment headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/* ************************************************************************
+/**************************************************************************
  * Copyright 2018 Advanced Micro Devices, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ************************************************************************/
+ **************************************************************************/
 
 #include "commands.h"  // has measurement_t
 #include "tests.h"     // for test_all
--- src/utilities.h
+++ src/utilities.h
@@ -1,4 +1,4 @@
-/* ************************************************************************
+/**************************************************************************
  * Copyright 2018 Advanced Micro Devices, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ************************************************************************/
+ **************************************************************************/
 
 #ifndef UTILITIES_H
 #define UTILITIES_H
