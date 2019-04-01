c983a2436804a56548eea46df6ab9c3eb42ab2a7
Brian Lilienthal <Brian.Lilienthal@amd.com>
2019-04-01 12:33:57 -0500

    Adding wget to module check in build script

--- build.sh
+++ build.sh
@@ -90,7 +90,7 @@ then
 else
     debug $LINENO ":" "Regular expression could not match: \n" "${OS_RELEASE}"
     echo "Distribution not recognized. Please manually install "\
-         "libelf libraries, make, zip, gcc, g++, and git." >&2
+         "libelf libraries, make, zip, gcc, g++, git, and wget." >&2
     exit 1
 fi
 
@@ -103,6 +103,7 @@ then
        [ "$(rpm -q 'make' 2>&1 | grep 'not installed')" != "" ]     ||
        [ "$(rpm -q 'gcc' 2>&1 | grep 'not installed')" != "" ]      ||
        [ "$(rpm -q 'zip' 2>&1 | grep 'not installed')" != "" ]      ||
+       [ "$(rpm -q 'wget' 2>&1 | grep 'not installed')" != "" ]     ||
        [ "$(rpm -q ${SSL_DEV} 2>&1 | grep 'not installed')" != "" ] ||
        [ "$(rpm -q ${GCC_CPP} 2>&1 | grep 'not installed')" != "" ]
     then
@@ -115,6 +116,7 @@ then
        [ "$(dpkg -l 'make' 2>&1 | grep 'no packages')" != "" ]     ||
        [ "$(dpkg -l 'gcc' 2>&1 | grep 'no packages')" != "" ]      ||
        [ "$(dpkg -l 'zip' 2>&1 | grep 'no packages')" != "" ]      ||
+       [ "$(dpkg -l 'wget' 2>&1 | grep 'no packages')" != "" ]     ||
        [ "$(dpkg -l ${SSL_DEV} 2>&1 | grep 'no packages')" != "" ] ||
        [ "$(dpkg -l ${GCC_CPP} 2>&1 | grep 'no packages')" != "" ]
     then
@@ -129,7 +131,7 @@ then
     debug $LINENO ":" "A dependency is missing, installing now."
     debug $LINENO ":" "Running Command: \"sudo ${INSTALLER} install -y git make gcc "\
           "zip ${SSL_DEV} ${GCC_CPP}\""
-    sudo ${INSTALLER} install -y git make gcc zip ${SSL_DEV} ${GCC_CPP}
+    sudo ${INSTALLER} install -y git make gcc zip wget ${SSL_DEV} ${GCC_CPP}
 fi
 
 # Rebuild SEV Tool binary
