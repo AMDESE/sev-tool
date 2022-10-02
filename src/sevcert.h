b1bae0b4c83d89f72a6e43fc16e0bc96210c3331
Radu Benea <kitanatahu@gmail.com>
2022-10-02 22:22:59 +0300

    declare {de,}compile_public_key* as static

--- src/sevcert.h
+++ src/sevcert.h
@@ -66,9 +66,9 @@ public:
     bool sign_with_key(uint32_t version, uint32_t pub_key_usage,
                        uint32_t pub_key_algorithm, EVP_PKEY **priv_key,
                        uint32_t sig1_usage, const SEV_SIG_ALGO sig1_algo);
-    SEV_ERROR_CODE compile_public_key_from_certificate(const sev_cert *cert,
+    static SEV_ERROR_CODE compile_public_key_from_certificate(const sev_cert *cert,
                                                        EVP_PKEY *evp_pub_key);
-    SEV_ERROR_CODE decompile_public_key_into_certificate(sev_cert *cert,
+    static SEV_ERROR_CODE decompile_public_key_into_certificate(sev_cert *cert,
                                                          EVP_PKEY *evp_pubkey);
     SEV_ERROR_CODE verify_sev_cert(const sev_cert *parent_cert1,
                                    const sev_cert *parent_cert2 = nullptr);
